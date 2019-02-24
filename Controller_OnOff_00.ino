50d7186ad6df4ad6226e0d59cabe1d82c9ecaa52
Ghiordy <cghiordy@gmail.com>
2019-02-23 21:15:48 -0500

    Add files via upload

---
+++ Controller_OnOff_00.ino
@@ -0,0 +1,43 @@
+#include<DHT.h>
+#include<DHT_U.h>
+// Supply
+const int RELE = 3;
+
+// DHT22 Temperature and humidity
+int SENSOR = 2;   
+float TEMPERATURA;
+float HUMEDAD;
+DHT dht(SENSOR, DHT22);
+
+//DT
+float DT;
+
+//Statistics variables
+int n = 10;
+int ts = 500;
+float Pred;
+int aj = 60;
+
+void setup() {
+  pinMode(RELE,OUTPUT);
+}
+
+void loop() {
+  Pred = 0;
+  DT = 0;
+  // Muestreo promedio
+  for (int i=0; i<n; i++){
+    //Getting temperature value
+    TEMPERATURA = dht.readTemperature();
+    DT = DT + TEMPERATURA;
+    delay(ts);
+  }
+  DT = DT / (n*ts) ;
+  // Getting humidity value
+  HUMEDAD = dht.readHumidity();
+  Pred = 60;
+  if(Pred < 37.5){
+    digitalWrite(RELE,HIGH);
+  }
+  else{digitalWrite(RELE,LOW);}
+}
